0e7c8196019f6d9836368140c46ad7a59cf3e965
Todd Albert <todd@bocacode.com>
2022-01-27 12:06:56 -0500

    connected to firestore and added a restaurant

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const restaurants = require('./restaurants.json');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-// connect to Firestore
+// import our credentials
 const credentials = require('./credentials.json');
 
+// connect to firebase services
+initializeApp({
+  credential: cert(credentials)
+});
+
+// connect to Firestore
+const db = getFirestore();
 
 // create a collection called "restaurants"
 
 // add each restaurant
+db.collection('restaurants').add(restaurants[1])
+  .then(doc => {
+    console.log('Added restaurant', doc.id);
+  })
+  .catch(err => {
+    console.error(err);
+  });
