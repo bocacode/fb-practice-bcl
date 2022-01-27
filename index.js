a12d9ad25dce2801a04d38bb9b6ee51446bb3821
Todd Albert <todd@bocacode.com>
2022-01-27 13:56:28 -0500

    playing with promises

--- index.js
+++ index.js
@@ -17,12 +17,42 @@ initializeApp({
 const db = getFirestore();
 
 // create a collection called "restaurants"
+console.log(' -- We are about to do some stuff -- ');
 
 // add each restaurant
-db.collection('restaurants').add(restaurants[1])
+db.collection('restaurants').add(restaurants[3])
   .then(doc => {
     console.log('Added restaurant', doc.id);
+    // the new restaurant has been
+    db.collection('restaurants').get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.id, ' => ', doc.data());
+      })
+    })
+    .then(() => {
+      console.log('All 4 restaurants returned.');
+    });
   })
   .catch(err => {
     console.error(err);
   });
+
+console.log(' -- This has nothing to do with restaurants -- ');
+// read one document
+// db.collection('restaurants').doc('hVJrR04ObLv6TyWAPMVp').get()
+//   .then(doc => {
+//     console.log(doc.id, ' => ', doc.data());
+//   })
+//   .catch(err => console.error(err));
+
+// get all documents
+// db.collection('restaurants').get()
+//   .then(snapshot => {
+//     snapshot.forEach(doc => {
+//       console.log(doc.id, ' => ', doc.data());
+//     })
+//   })
+//   .catch(console.error);
+
+// find a document(s)
