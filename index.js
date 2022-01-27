163aea9f91220e71e00dee496ab8c34003681efc
Todd Albert <todd@bocacode.com>
2022-01-27 11:44:51 -0500

    added firebase admin and credentials

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // import our restaurants
 const restaurants = require('./restaurants.json');
 
-console.log(restaurants[0].name)
+// import a set of tools to talk to firebase and Firestore
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
 // connect to Firestore
+const credentials = require('./credentials.json');
+
 
 // create a collection called "restaurants"
 
